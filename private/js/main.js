f368189efd8f7e9e290d38477790f507e41d1a17
cerivan <mimoun.fellah@voila.fr>
2014-09-15 23:17:22 +0200

    onfail

--- private/js/main.js
+++ private/js/main.js
@@ -16,7 +16,9 @@
       navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
         destinationType: destinationType.DATA_URL });
     }
-	 
+	 function onFail(message) {
+      alert('Failed because: ' + message);
+    }
 
     // Called when a photo is successfully retrieved
     //
