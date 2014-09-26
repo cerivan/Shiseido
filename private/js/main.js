13b45074876f1f5e0b1e0b6ddf42a733436c9968
cerivan <github@cerivan.com>
2014-09-26 11:59:28 +0200

    Update main.js

--- private/js/main.js
+++ private/js/main.js
@@ -13,7 +13,7 @@
     }
 	function capturePhoto() {
       // Take picture using device camera and retrieve image as base64-encoded string
-      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+      navigator.camera.getPicture(onPhotoDataSuccess, uploadPhoto, onFail, { quality: 50,
         destinationType: destinationType.DATA_URL });
     }
 	 function onFail(message) {
